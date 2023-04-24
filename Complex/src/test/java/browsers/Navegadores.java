9eab9ea69e22e135ca2035d0e8495c711c5e5e6a
Igor Roberth <96749911+IgorRoberth@users.noreply.github.com>
2023-04-24 18:54:46 -0300

    Atualizar o Navegadores.java

--- Complex/src/test/java/browsers/Navegadores.java
+++ Complex/src/test/java/browsers/Navegadores.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-
 import driver.Driver;
 
 public class Navegadores extends Driver {
