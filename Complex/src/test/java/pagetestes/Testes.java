9dbee65d15fc76388910302d9ba2252fe34e169a
Igor Roberth <igorroberthm@gmail.com>
2023-07-03 12:37:51 -0300

    Mantido o código e atualizado versão do Driver Chrome

--- Complex/src/test/java/pagetestes/Testes.java
+++ Complex/src/test/java/pagetestes/Testes.java
@@ -26,105 +26,108 @@ public class Testes {
 	private Navegadores navegadores;
 	private WebDriver driver;
 
-    @Before
-    public void InicioTeste() {
-        navegadores = new Navegadores("Chrome");
-        driver = navegadores.configurarDriver();
-        String url = ConfigUrl.getURL();
-        driver.get(url);
-    }
+	@Before
+	public void InicioTeste() {
+		navegadores = new Navegadores("Chrome");
+		driver = navegadores.configurarDriver();
+		String url = ConfigUrl.getURL();
+		driver.get(url);
+	}
 
-    private void ExecutarCompraEValidarProdutoEmsgFinal(String login_user, String senha_user, By button,
-                                                        String validar_produto, String nome_cliente,
-                                                        String sobrenome_cliente, String cep_cliente, String msg_final)
-            throws IOException {
+	private void ExecutarCompraEValidarProdutoEmsgFinal(String login_user, String senha_user, By button,
+			String validar_produto, String nome_cliente, String sobrenome_cliente, String cep_cliente, String msg_final)
+			throws IOException {
 
-        Metodos.escrever(driver, Elementos.Login.login, login_user);
-        Metodos.escrever(driver, Elementos.Login.senha, senha_user);
-        Metodos.clicar(driver, Elementos.Login.button);
-        Metodos.clicar(driver, selecionarItem.validarItem);
-        Metodos.clicar(driver, selecionarItem.adcCarrinho);
-        Metodos.clicar(driver, selecionarItem.carrinho);
-        Metodos.validarItem(driver, selecionarItem.validarItem, validar_produto);
-        Metodos.clicar(driver, selecionarItem.check);
-        Metodos.escrever(driver, DadosPessoais.nome, nome_cliente);
-        Metodos.escrever(driver, DadosPessoais.sobrenome, sobrenome_cliente);
-        Metodos.escrever(driver, DadosPessoais.cep, cep_cliente);
-        Metodos.clicar(driver, DadosPessoais.Continue);
-        Metodos.clicar(driver, DadosPessoais.finish);
-        Metodos.validarItem(driver, DadosPessoais.msgFinal, msg_final);
-        System.out.println("\n=====Produto " + validar_produto + " e mensagem " + msg_final + " validado com sucesso=====");
-    }
+		Metodos.escrever(driver, Elementos.Login.login, login_user);
+		Metodos.escrever(driver, Elementos.Login.senha, senha_user);
+		Metodos.clicar(driver, Elementos.Login.button);
+		Metodos.clicar(driver, selecionarItem.validarItem);
+		Metodos.clicar(driver, selecionarItem.adcCarrinho);
+		Metodos.clicar(driver, selecionarItem.carrinho);
+		Metodos.validarItem(driver, selecionarItem.validarItem, validar_produto);
+		Metodos.clicar(driver, selecionarItem.check);
+		Metodos.escrever(driver, DadosPessoais.nome, nome_cliente);
+		Metodos.escrever(driver, DadosPessoais.sobrenome, sobrenome_cliente);
+		Metodos.escrever(driver, DadosPessoais.cep, cep_cliente);
+		Metodos.clicar(driver, DadosPessoais.Continue);
+		Metodos.clicar(driver, DadosPessoais.finish);
+		Metodos.validarItem(driver, DadosPessoais.msgFinal, msg_final);
+		System.out.println(
+				"\n=====Produto " + validar_produto + " e mensagem " + msg_final + " validado com sucesso=====");
+	}
 
-    @Test
-    public void ExecutarCompraEValidarProdutoEmsgFinal() throws IOException {
+	@Test
+	public void ExecutarCompraEValidarProdutoEmsgFinal() throws IOException {
 
-        String login_user = "standard_user";
-        String senha_user = "secret_sauce";
-        By button = null;
-        String validar_produto = "Sauce Labs Backpack";
-        String nome_cliente = "Cláudio Emanuel";
-        String sobrenome_cliente = "José da Rocha";
-        String cep_cliente = "78559706";
-        String msg_final = "Thank you for your order!";
+		String login_user = "standard_user";
+		String senha_user = "secret_sauce";
+		By button = null;
+		String validar_produto = "Sauce Labs Backpack";
+		String nome_cliente = "Cláudio Emanuel";
+		String sobrenome_cliente = "José da Rocha";
+		String cep_cliente = "78559706";
+		String msg_final = "Thank you for your order!";
 
-        ExecutarCompraEValidarProdutoEmsgFinal(login_user, senha_user, button, validar_produto, nome_cliente,
-                sobrenome_cliente, cep_cliente, msg_final);
+		ExecutarCompraEValidarProdutoEmsgFinal(login_user, senha_user, button, validar_produto, nome_cliente,
+				sobrenome_cliente, cep_cliente, msg_final);
 
-    }
+	}
 
-    private void ValidarTentativaDeLoginComUsuarioBloqueado(String UserBloq, String senha_Bloq, By button, String msgErro) {
+	private void ValidarTentativaDeLoginComUsuarioBloqueado(String UserBloq, String senha_Bloq, By button,
+			String msgErro) {
 
-        Metodos.escrever(driver, Elementos.Login.login, UserBloq);
-        Metodos.escrever(driver, Elementos.Login.senha, senha_Bloq);
-        Metodos.clicar(driver, Elementos.Login.button);
-        Metodos.validarItem(driver, User_Bloq.validarErro, msgErro);
-        System.out.println("\n======Mensagem de erro validado com sucesso: " + msgErro + "======");
+		Metodos.escrever(driver, Elementos.Login.login, UserBloq);
+		Metodos.escrever(driver, Elementos.Login.senha, senha_Bloq);
+		Metodos.clicar(driver, Elementos.Login.button);
+		Metodos.validarItem(driver, User_Bloq.validarErro, msgErro);
+		System.out.println("\n======Mensagem de erro validado com sucesso: " + msgErro + "======");
 
-    }
+	}
 
-    @Test
-    public void ValidarTentativaDeLoginComUsuarioBloqueado() throws IOException, InterruptedException {
-    	
-        String User_Bloq = "locked_out_user";
-        String senha_Bloq = "secret_sauce";
-        By button = null;
-        String msgErro = "Epic sadface: Sorry, this user has been locked out.";
+	@Test
+	public void ValidarTentativaDeLoginComUsuarioBloqueado() throws IOException, InterruptedException {
 
-        ValidarTentativaDeLoginComUsuarioBloqueado(User_Bloq, senha_Bloq, button, msgErro);
-    }
+		String User_Bloq = "locked_out_user";
+		String senha_Bloq = "secret_sauce";
+		By button = null;
+		String msgErro = "Epic sadface: Sorry, this user has been locked out.";
 
-    private void ValidarCompraComVerificacaoDeProdutoEntregueIncorretamente(String login_Probl, String senha_Probl,
-                                                                           String sauce_Labs, String prod_retornado, By button) throws IOException {
+		ValidarTentativaDeLoginComUsuarioBloqueado(User_Bloq, senha_Bloq, button, msgErro);
+	}
 
-        Metodos.escrever(driver, Elementos.Login.login, login_Probl);
-        Metodos.escrever(driver, Elementos.Login.senha, senha_Probl);
-        Metodos.clicar(driver, Elementos.Login.button);
-        Metodos.clicar(driver, UserProblem.sauceLabs);
-        Metodos.validarItem(driver, UserProblem.produtoRetornado, prod_retornado);
-        System.out.println("\n====Erro, o produto selecionado era " + sauce_Labs + " e o retorno foi " + prod_retornado + "====");
+	private void ValidarCompraComVerificacaoDeProdutoEntregueIncorretamente(String login_Probl, String senha_Probl,
+			String sauce_Labs, String prod_retornado, By button) throws IOException {
 
-    }
+		Metodos.escrever(driver, Elementos.Login.login, login_Probl);
+		Metodos.escrever(driver, Elementos.Login.senha, senha_Probl);
+		Metodos.clicar(driver, Elementos.Login.button);
+		Metodos.clicar(driver, UserProblem.sauceLabs);
+		Metodos.validarItem(driver, UserProblem.produtoRetornado, prod_retornado);
+		System.out.println(
+				"\n====Erro, o produto selecionado era " + sauce_Labs + " e o retorno foi " + prod_retornado + "====");
 
-    @Test
-    public void ValidarCompraComVerificacaoDeProdutoEntregueIncorretamente() throws IOException {
+	}
 
-        String login_Probl = "problem_user";
-        String senha_Probl = "secret_sauce";
-        By button = null;
-        String sauce_Labs = "Sauce Labs Onesie";
-        String prod_retornado = "Test.allTheThings() T-Shirt (Red)";
+	@Test
+	public void ValidarCompraComVerificacaoDeProdutoEntregueIncorretamente() throws IOException {
 
-        ValidarCompraComVerificacaoDeProdutoEntregueIncorretamente(login_Probl, senha_Probl, sauce_Labs, prod_retornado,
-                button);
+		String login_Probl = "problem_user";
+		String senha_Probl = "secret_sauce";
+		By button = null;
+		String sauce_Labs = "Sauce Labs Onesie";
+		String prod_retornado = "Test.allTheThings() T-Shirt (Red)";
 
-    }
+		ValidarCompraComVerificacaoDeProdutoEntregueIncorretamente(login_Probl, senha_Probl, sauce_Labs, prod_retornado,
+				button);
 
-    @After
-    public void tearDown() {
- 
-    	if(driver!=null) {
-    		driver.quit();    	}
-      
-    }
+	}
+
+	@After
+	public void tearDown() {
+
+		if (driver != null) {
+			driver.quit();
+
+		}
+	}
 }
